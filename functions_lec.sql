0f1b3aa1e8186ed83680755b35660e6af746cc50
Javier Ruedas <jruedas@twc.com>
2023-11-29 14:15:55 -0600

    complete aggregate function lecture

--- functions_lec.sql
+++ functions_lec.sql
@@ -11,3 +11,22 @@ DESCRIBE employees;
 SELECT CONCAT(month(birth_date), ' ', DAY(birth_date)) AS birthday, hire_date FROM employees ORDER BY hire_date DESC LIMIT 50;
 
 SELECT DATEDIFF(hire_date, CURDATE()) / 365, hire_date FROM employees WHERE first_name = 'Maya' ORDER BY DATEDIFF(hire_date, CURDATE());
+
+SELECT COUNT(*) FROM employees;
+
+SELECT gender, COUNT(*) FROM employees GROUP BY gender;
+
+SELECT last_name, COUNT(last_name) AS quantity FROM employees GROUP BY last_name;
+
+USE codeup_test_db;
+
+SELECT AVG(sales) FROM albums;
+
+SELECT artist, AVG(sales) FROM albums GROUP BY artist ORDER BY AVG(sales) DESC;
+
+SELECT artist, TRUNCATE(AVG(sales), 2) AS average_sales FROM albums GROUP BY artist ORDER BY average_sales DESC;
+
+USE employees;
+
+SELECT TRUNCATE(MAX(DATEDIFF(hire_date, birth_date)/365), 2) FROM employees;
+SELECT TRUNCATE(MIN(DATEDIFF(hire_date, birth_date)/365), 2) FROM employees;
